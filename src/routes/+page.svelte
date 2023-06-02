42d8ab5a9de2b2fec8e2ae9f1197ea609cce364c
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-06-02 13:25:34 +0200

    trying to fix build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
 	// Sections
-	import About from '$lib/sections/about.svelte';
-	import Projects from '$lib/sections/projects.svelte';
-	import Service from '$lib/sections/service.svelte';
-	import Contact from '$lib/sections/contact.svelte';
+	import About from '$lib/components/sections/about.svelte';
+	import Projects from '$lib/components/sections/projects.svelte';
+	import Service from '$lib/components/sections/service.svelte';
+	import Contact from '$lib/components/sections/contact.svelte';
 </script>
 
 <div class="flex flex-col gap-40 h-full">
