1486699e7881c2e00d690e8660988cb031bd12d8
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-06-05 12:21:17 +0200

    UI changes

--- src/lib/components/form/contactForm.svelte
+++ src/lib/components/form/contactForm.svelte
@@ -4,28 +4,26 @@
 </script>
 
 <!-- HTML -->
-<section class="h-fit flex flex-col gap-5 mx-2">
-	<form class="flex flex-col gap-5">
-		<div>
-			<Label for="name" class="mb-2">Name</Label>
-			<Input type="text" id="name" placeholder="Name" size="md" />
-		</div>
+<form class="flex flex-col gap-5">
+	<div>
+		<Label for="name" class="mb-2">Name</Label>
+		<Input type="text" id="name" placeholder="Name" size="md" />
+	</div>
 
-		<div>
-			<Label for="email" class="mb-2">Email</Label>
-			<Input type="email" id="email" placeholder="Email" size="md" />
-		</div>
+	<div>
+		<Label for="email" class="mb-2">Email</Label>
+		<Input type="email" id="email" placeholder="Email" size="md" />
+	</div>
 
-		<div>
-			<Label for="message" class="mb-2">Your message</Label>
-			<Textarea id="message" placeholder="Your message" rows="4" name="message" />
-		</div>
+	<div>
+		<Label for="message" class="mb-2">Your message</Label>
+		<Textarea id="message" placeholder="Your message" rows="4" name="message" />
+	</div>
 
-		<div>
-			<Button type="submit">Submit</Button>
-		</div>
-	</form>
-</section>
+	<div>
+		<Button type="submit">Submit</Button>
+	</div>
+</form>
 
 <!-- STYLE -->
 <style>
--- src/lib/components/sections/contact.svelte
+++ src/lib/components/sections/contact.svelte
@@ -8,9 +8,13 @@
 <section class="w-full" id="contact">
 	<div class="flex container flex-col gap-10 mx-auto">
 		<div class="mx-4">
-			<Heading tag="h2" customSize="text-5xl font-extrabold text-center"><i class="fa-solid fa-envelope" /> Contact</Heading>
-			<p class="text-center">Have a website dream? Contact me and lets make that dream come true</p>
-			<ContactForm />
+			<div class="flex container flex-col gap-10">
+				<div class="flex flex-col gap-4">
+					<Heading tag="h2" customSize="text-5xl font-extrabold text-center"><i class="fa-solid fa-envelope" /> Contact</Heading>
+					<p class="text-center">Have a website dream? Contact me and lets make that dream come true</p>
+				</div>
+				<ContactForm />
+			</div>
 		</div>
 	</div>
 </section>
